c3f8548663cc25d41908aad83ec7cf2663f1b358
Denis Antonchyk <tenirey11@gmail.com>
2025-10-18 15:49:09 +0300

    feat(category): add graphql query for getting a category by ID
    - Add new query mapping method in CategoryController to get a category by ID
    - Implement new GET_CATEGORY_BY_ID GraphQL query in category.graphqls
    - Update the existing GET_ALL_CATEGORIES query to return a list of converted categories

--- server/src/main/java/com/techdeliver/controller/CategoryController.java
+++ server/src/main/java/com/techdeliver/controller/CategoryController.java
@@ -1,47 +1,53 @@
 package com.techdeliver.controller;
 
 import com.techdeliver.dto.ProductCategoryDto;
+import com.techdeliver.request.AddCategoryRequest;
 import com.techdeliver.service.category.IProductCategoryService;
+import com.techdeliver.service.category.ProductCategoryService;
 import com.techdeliver.util.CategoryMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 public class CategoryController {
 
     private final IProductCategoryService categoryService;
+    private final ProductCategoryService productCategoryService;
 
-//    @QueryMapping
-//    public CategoryListResponse getAllCategories() {
-//        var categories = categoryService.getAllCategories();
-//
-//        return new CategoryListResponse(
-//                categories.stream()
-//                .map(CategoryMapper::mapToDto).toList(),
-//                "Get all categories success!");
-//    }
-//
-//    @QueryMapping
-//    public List<ProductCategoryDto> categories() {
-//        var categories = categoryService.getAllCategories();
-//        return categories.stream()
-//                .map(CategoryMapper::mapToDto).toList();
-//    }
+    @QueryMapping
+    public CategoryListResponse getAllCategories() {
+        var categories = categoryService.getAllCategories();
 
+        return new CategoryListResponse(
+                productCategoryService.getConvertedCategories(categories),
+                "Get all categories success!");
+    }
 
-//    @QueryMapping
-//    public CategoryResponse getCategoryByName(@Argument String name) {
-//
-//        var category = categoryService.getCategoryByName(name);
-//
-//        return CategoryResponse(CategoryMapper.mapToDto(category), )
-//
-//
-//    }
+    @QueryMapping
+    public CategoryResponse getCategoryById(@Argument UUID categoryId) {
+
+        var category = categoryService.getCategoryById(categoryId);
+
+        return new CategoryResponse(
+                productCategoryService.convertToDto(category),
+                "Get category success!" );
+    }
+
+    @MutationMapping
+    public CategoryResponse createCategory(@Argument AddCategoryRequest input) {
+
+        var createdCategory = categoryService.createCategory(input);
+        return new CategoryResponse(
+                productCategoryService.convertToDto(createdCategory),
+                "Create category success!");
+    }
 
     public record CategoryResponse(ProductCategoryDto categoryDto,
                                    String message) {}
