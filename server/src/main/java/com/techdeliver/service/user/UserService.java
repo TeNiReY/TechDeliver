0b1e14af102530de9b8a235fce766e80292295b3
Denis Antonchyk <tenirey11@gmail.com>
2025-10-18 22:58:25 +0300

    feat(user-service): add getUserById method`    - Implemented a new method to retrieve a user by its ID.    - This method throws a `ResourceNotFoundException` if the user is not found.
    2. `feat(user-service): add updateUsername method`
       - Implemented a method to update a user's username if it's unique.
       - If the provided new username already exists, an `AlreadyExistsException` is thrown.
    
    3. `feat(user-service): add updatePassword method`
       - Implemented a method to update a user's password with the provided old and new passwords.
       - If the old password is incorrect, an `InvalidCredentialsException` is thrown.
    
    4. `feat(user-service): add getConvertedProducts and convertToDto methods`
       - Implemented new methods to convert a list of `UserEntity` objects to a list of `UserDto` objects.
       - The `getConvertedProducts` method converts a list of `UserEntity` objects and returns a list of `UserDto` objects.
       - The `convertToDto` method takes a single `UserEntity` object and returns a `UserDto` object.

--- server/src/main/java/com/techdeliver/service/user/UserService.java
+++ server/src/main/java/com/techdeliver/service/user/UserService.java
@@ -1,14 +1,20 @@
 package com.techdeliver.service.user;
 
+import com.techdeliver.dto.UserDto;
 import com.techdeliver.entity.UserEntity;
 import com.techdeliver.exception.AlreadyExistsException;
+import com.techdeliver.exception.InvalidCredentialsException;
+import com.techdeliver.exception.ResourceNotFoundException;
 import com.techdeliver.repository.UserRepository;
 import com.techdeliver.request.RegisterRequest;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +22,14 @@ public class UserService implements IUserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    private final ModelMapper modelMapper;
+
+    @Override
+    public UserEntity getUserById(UUID userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(()
+                        -> new ResourceNotFoundException("User not found with id: " + userId));
+    }
 
     @Override
     public UserEntity createUserAccount(RegisterRequest request) {
@@ -30,6 +44,50 @@ public class UserService implements IUserService {
                     return userRepository.save(userEntity);
                 }).orElseThrow(() ->
                         new AlreadyExistsException("User with the same credentials already exists!"));
+    }
+
+    @Override
+    public UserEntity updateUsername(UUID userId, String newUsername) { //make this as a transaction
+        return Optional.ofNullable(getUserById(userId))
+                .map(u -> {
+                    if (!u.getUsername().equals(newUsername) &&
+                            !userRepository.existsByUsername(newUsername)) {
+                        u.setUsername(newUsername);
+                        return userRepository.save(u);
+                    } else {
+                        throw new AlreadyExistsException("User with the same username already exists!");
+                    }
+                }).orElseThrow(() ->
+                        new ResourceNotFoundException("User not found with id: " + userId)); //TODO: check logic here
+    }
+
+    @Override
+    public UserEntity updatePassword(UUID userId, String oldPassword, String newPassword) {
+        return Optional.ofNullable(getUserById(userId))
+                .map(u -> {
+                    if (!passwordEncoder.matches(oldPassword, u.getPassword())) {
+                        throw new InvalidCredentialsException("Invalid password!");
+                    }
+
+                    u.setPassword(passwordEncoder.encode(newPassword));
+                    return userRepository.save(u);
+                }).orElseThrow(() ->
+                        new ResourceNotFoundException("User not found with id: " + userId)); //TODO: check logic here
+    }
+
+    @Override
+    public List<UserDto> getConvertedProducts(List<UserEntity> users) {
+        return users.stream().map(this::convertToDto).toList();
+    }
+
+    @Override
+    public UserDto convertToDto(UserEntity user) {
+        UserDto userDto = new UserDto();
+        userDto.setUserId(user.getUserId());
+        userDto.setUsername(user.getUsername());
+        userDto.setEmail(user.getEmail());
+        // Не включаем cart и roles пока что, чтобы избежать ошибок
+        return userDto;
     }
 
 
