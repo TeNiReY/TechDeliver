f3243516cf991d36c11bda9ed8d3ad7d665b56c1
Denis Antonchyk <tenirey11@gmail.com>
2025-10-18 15:34:38 +0300

    feat(request, dto): allow passing category name or ID when adding a product
    - Update `AddProductRequest` to accept a `categoryNameOrId` string instead of `ProductCategoryEntity`
    - Update `ProductCategoryDto` to include `categoryName` and `categoryDescription` properties

--- server/src/main/java/com/techdeliver/dto/ProductCategoryDto.java
+++ server/src/main/java/com/techdeliver/dto/ProductCategoryDto.java
@@ -10,8 +10,8 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductCategoryDto {
-    private UUID id;
-    private String name;
-    private String description;
+    private UUID categoryId;
+    private String categoryName;
+    private String categoryDescription;
     private List<ProductDto> products;
 }
