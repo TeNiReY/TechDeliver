922134fe948c681ec73aa67b408d27ea5406965e
Denis Antonchyk <tenirey11@gmail.com>
2025-10-18 18:30:30 +0300

    feat(security): implement JWT-based authentication system
    - Add JWTUtils class for token generation, validation, and retrieving user information
    - Implement AuthTokenFilter to authenticate incoming requests and set the authentication object in SecurityContextHolder
    - Add JwtAuthEntryPoint to handle unauthenticated requests
    - Update SecurityConfig to include the new filter and entry point, as well as configure session management and exception handling
    - Update dependencies in pom.xml for required JWT and Spring Security dependencies

---
+++ server/src/main/java/com/techdeliver/security/user/ShopUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.techdeliver.security.user;
+
+import com.techdeliver.entity.UserEntity;
+import com.techdeliver.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = Optional.ofNullable(userRepository.findByUsername(username))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
+
+}
