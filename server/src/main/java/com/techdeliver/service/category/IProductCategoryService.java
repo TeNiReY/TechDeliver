d9bbd487be2582dfe2fb27385587ac65939fbf6f
Denis Antonchyk <tenirey11@gmail.com>
2025-10-18 15:35:07 +0300

    feat(repository): add existsByCategoryName method and findByCategoryNameOrCategoryId
    - Adds a method to check if a product category with the given name exists.
    - Modifies the findByCategoryName method to accept categoryId as an additional parameter.

--- server/src/main/java/com/techdeliver/service/category/IProductCategoryService.java
+++ server/src/main/java/com/techdeliver/service/category/IProductCategoryService.java
@@ -1,17 +1,27 @@
 package com.techdeliver.service.category;
 
+import com.techdeliver.dto.ProductCategoryDto;
 import com.techdeliver.entity.ProductCategoryEntity;
+import com.techdeliver.request.AddCategoryRequest;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface IProductCategoryService {
 
 
+    ProductCategoryEntity getCategoryById(UUID categoryId);
 
     Optional<ProductCategoryEntity> getCategoryByName(String categoryName);
 
-    ProductCategoryEntity createCategory(String categoryName);
+    ProductCategoryEntity createCategory(AddCategoryRequest request);
+
+    ProductCategoryEntity addCategory(String categoryName);
 
     List<ProductCategoryEntity> getAllCategories();
+
+    List<ProductCategoryDto> getConvertedCategories(List<ProductCategoryEntity> categories);
+
+    ProductCategoryDto convertToDto(ProductCategoryEntity category);
 }
