b85a74560faeb47d8b0126eddfa68732bf040c16
Denis Antonchyk <tenirey11@gmail.com>
2025-10-18 22:59:18 +0300

    feat(user, controller): add update username and password mutations
    - Implement updateUsername and updateUserPassword mutations in the UserController
    - Update user.graphqls to include new mutations

--- server/src/main/java/com/techdeliver/controller/UserController.java
+++ server/src/main/java/com/techdeliver/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.techdeliver.controller;
 
+import com.techdeliver.dto.UserDto;
 import com.techdeliver.request.RegisterRequest;
 import com.techdeliver.service.user.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,6 +21,21 @@ public class UserController {
     public RegisterResponse registerUserAccount(@Argument RegisterRequest input) {
         userService.createUserAccount(input);
         return new RegisterResponse(true, "User Account Created Successfully");
+    }
+
+    @MutationMapping
+    public UserDto updateUsername(@Argument UUID userId, @Argument String newUsername) {
+        var updatedUser = userService.updateUsername(userId, newUsername);
+        return userService.convertToDto(updatedUser);
+    }
+
+    @MutationMapping
+    public UserDto updateUserPassword(@Argument UUID userId,
+                                  @Argument String oldPass,
+                                  @Argument String newPass
+                                  ) {
+        var updatedUser = userService.updatePassword(userId, oldPass, newPass);
+        return userService.convertToDto(updatedUser);
     }
 
 
