cef89a923b6f0148c5c08a8dc537ba85484a3942
Denis Antonchyk <tenirey11@gmail.com>
2025-10-19 22:06:39 +0300

    feat(cart): add mutations for managing cart items (add, remove, update quantity)

--- server/src/main/java/com/techdeliver/controller/CartController.java
+++ server/src/main/java/com/techdeliver/controller/CartController.java
@@ -4,6 +4,7 @@ import com.techdeliver.dto.CartDto;
 import com.techdeliver.service.cart.ICartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -20,5 +21,34 @@ public class CartController {
         var cart = cartService.getCartByUserId(userId);
         return cartService.convertToDto(cart);
     }
+
+    @MutationMapping //TODO: check this 3 methods
+    public CartResponse addItemToCart(@Argument UUID userId,
+                                      @Argument UUID productId,
+                                      @Argument int quantity
+    ) {
+        cartService.addItemToCart(userId, productId, quantity);
+        return new CartResponse(true, "Item added successfully");
+    }
+
+    @MutationMapping
+    public CartResponse removeItemFromCart(@Argument UUID userId,
+                                      @Argument UUID productId
+    ) {
+        cartService.removeItemFromCart(userId, productId);
+        return new CartResponse(true, "Item removed successfully");
+    }
+
+    @MutationMapping
+    public CartResponse updateItemQuantity(@Argument UUID userId,
+                                      @Argument UUID productId,
+                                      @Argument int newQuantity
+    ) {
+        cartService.updateItemQuantity(userId, productId, newQuantity);
+        return new CartResponse(true, "Item quantity updated successfully");
+    }
+
+        public record CartResponse(boolean result,
+                               String message) {}
 
 }
