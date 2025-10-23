6a04c3c5c3086db671642a6bddf8160e833753d3
Denis Antonchyk <tenirey11@gmail.com>
2025-10-24 02:47:11 +0300

    feat(util): introduce InstallationPriceCalculator service
    - Implement a service to calculate installation prices based on product categories' installation complexity coefficients.
    - The basic installation price is set through a method for easy configuration.

---
+++ server/src/main/java/com/techdeliver/util/InstallationPriceCalculator.java
@@ -0,0 +1,31 @@
+package com.techdeliver.util;
+
+import com.techdeliver.entity.CartItemEntity;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Service
+public class InstallationPriceCalculator {
+
+    private BigDecimal basicInstallationPrice = BigDecimal.valueOf(20);
+
+    public void setBasicInstallationPrice(BigDecimal basicInstallationPrice) {
+        this.basicInstallationPrice = basicInstallationPrice;
+    }
+
+    public BigDecimal getInstallationPrice() {
+        return basicInstallationPrice;
+    }
+
+    public BigDecimal calculateInstallationPrice(Set<CartItemEntity> cartItems) {
+        return cartItems.stream()
+                .map(cartItem -> basicInstallationPrice
+                        .multiply(BigDecimal.valueOf(
+                                cartItem.getProduct().getProductCategory()
+                                        .getInstallationComplexityCoefficient())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+}
